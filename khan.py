fad1ffacb6cc24d858687e05ea112fad574ad361
Zahidsultana <107804406+Zahidsultana@users.noreply.github.com>
2022-06-29 11:36:44 +0500

    Update khan.py
    git clone https://github.com/Zahidsultana/khan.git
    ls
    cd khan
    python2 khan.py

--- khan.py
+++ khan.py
@@ -1,3 +1,38 @@
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes To lovehacker
+#If You Wanna Take Credits For This Code, Please Look Yourself Again...
+#Reserved2020
+import os,sys,time,mechanize,itertools,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
+from multiprocessing.pool import ThreadPool
+
+####  RANDOM Clour ####
+P  = '\033[1;97m'  #
+M  = '\033[1;91m' #
+H  = '\033[1;92m' #
+K = '\033[1;93m' #
+B  = '\033[1;94m' #
+U  = '\033[1;95m' #
+O = '\033[1;96m' #
+
+my_color = [P, M, H, K, B, U, O]
+warna = random.choice(my_color)
+warni = random.choice(my_color)
+def pkgs():
+        love("\033[1;91m«-----------------\033[1;96mSHABIR BALOCH\033[1;91m-----------------»")
+        love("\033[1;96m«-----------------Disclaimer---------------»")
+        love("\033[1;91m     This Tool is for Educational Purpose")
+        love("\033[1;93mThis presentation is for educational")
+        love("\033[1;93mpurposes ONLY.How you use this information")
+        love("\033[1;93mis your responsibility.I will not be")
+        love("\033[1;93mheld accountable This Tool/Channel Doesn't")
+        love("\033[1;93mSupport illegal activities.for any illegal")
+        love("\033[1;93mActivitie This Tool is for Educational Purpose")
+        love("\033[1;91m«------------------SHABIR BALOCH----------------»")
+        love("\033[1;95mB4Baloch 2nd Tool Start ComingSoon New Update»")
+        love("\033[1;96m «-----------------\033[1;92mSHABIR BALOCH\033[1;96m--------------»")
+        time.sleep(0.3)
+        os.system("pip install lolcat")
 echo "# khan" >> README.md
 git init
 git add README.md
